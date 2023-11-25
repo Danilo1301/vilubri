95585127191167314a89e1fcf067fbd0d1a70c20
Danilo <danilomaioli@gmail.com>
2023-11-25 19:17:12 -0300

    added nav bar

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../../../public/logo-vilubri.png';
 
 interface IItem {
     title: string
@@ -66,6 +67,16 @@ function Home() {
        
     return (
         <div className='container-fluid'>
+            <div className="nav row">
+                <div className='col'>
+                    <img className="nav-image p-2" src="logo-vilubri.png"></img>
+                </div>
+                <div className='col align-self-center'>
+                    <div className='nav-alert p-2 shadow-lg text-center'>
+                        Aviso
+                    </div>
+                </div>
+            </div>
             <div className='row row-cols-1 row-cols-sm-2'>
                 <Item item={item}/>
                 <Item item={item}/>
